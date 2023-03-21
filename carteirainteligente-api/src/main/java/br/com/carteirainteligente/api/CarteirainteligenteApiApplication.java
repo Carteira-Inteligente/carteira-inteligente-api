202a507f08326d66b8c80b668204ebb18d185572
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2023-03-21 19:32:53 -0300

    estrutura básica do sistema

---
+++ carteirainteligente-api/src/main/java/br/com/carteirainteligente/api/CarteirainteligenteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carteirainteligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteirainteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteirainteligenteApiApplication.class, args);
+	}
+
+}
---
+++ carteirainteligente-api/src/test/java/br/com/carteirainteligente/api/CarteirainteligenteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carteirainteligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteirainteligenteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
