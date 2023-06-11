57f7c92b8b1c7adc74fcf0bb5fe5445b9793fd5f
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2023-06-11 16:44:24 -0300

    chatgpt

--- carteirainteligente-api/src/main/java/br/com/carteirainteligente/api/controller/ChatGPTController.java
+++ carteirainteligente-api/src/main/java/br/com/carteirainteligente/api/controller/ChatGPTController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/chatgpt")
-public class ChatGPTAPI {
+public class ChatGPTController {
 
     @GetMapping
     public void listEntries() {
